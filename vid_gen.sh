a790812525db7899452ed653766cd14cab898ff9
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2025-04-30 21:48:19 +0530

    VID GEN BIG TIME

--- vid_gen.sh
+++ vid_gen.sh
@@ -10,7 +10,8 @@ python3 replay.py \
  --mp4 \
  --starting-fuel 200 \
  --wind-badness 10 \
-  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/fuel_model_200_fuel/sb3_ppo_model_160.zip
+  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/fuel_model_200_fuel/sb3_ppo_model_160.zip \
+  --savetraj
 echo "vid1: done"
 python3 replay.py \
  --model ppo_sb3 \
@@ -24,5 +25,53 @@ python3 replay.py \
  --mp4 \
  --starting-fuel 10000 \
  --wind-badness 10 \
-  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/fuel_model_bad_max_fuel/sb3_ppo_model_120.zip
+  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/fuel_model_bad_max_fuel/sb3_ppo_model_120.zip \
+  --savetraj
 echo "vid2: done"
+python3 replay.py \
+ --model ppo_sb3 \
+ --curr-stage-entry-point max \
+ --curr-stages 50 \
+ --skip-frames 100 \
+ --episodes 1 \
+ --sleep 1 \
+ --scenario MvaGoAroundScenario \
+ --num-airplanes 1 \
+ --mp4 \
+ --starting-fuel 10000 \
+ --wind-badness 10 \
+  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/goaround_sb3_ppo_model_120.zip \
+  --savetraj
+
+python3 replay.py \
+ --model ppo_sb3 \
+ --curr-stage-entry-point max \
+ --curr-stages 100 \
+ --skip-frames 100 \
+ --episodes 1 \
+ --sleep 1 \
+ --scenario SupaSupa \
+ --num-airplanes 1 \
+ --mp4 \
+ --starting-fuel 10000 \
+ --wind-badness 5 \
+  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/cute_model_supasupa_trainedtill_stage98/curr_model_stage98_entry114.35479633652099_81.02785148201173_hdg247_ep40.zip \
+  --savetraj
+
+python3 replay.py \
+ --model ppo_sb3 \
+ --curr-stage-entry-point max \
+ --curr-stages 100 \
+ --skip-frames 100 \
+ --entry 80,100 \
+ --level 250 \
+ --heading 135 \
+ --episodes 1 \
+ --sleep 1 \
+ --scenario SupaSupa \
+ --num-airplanes 1 \
+ --mp4 \
+ --starting-fuel 10000 \
+ --wind-badness 5 \
+  --checkpoint /Users/pjr/Desktop/Plaksha/sem6/RL/project/atc-lite/final_models/cute_model_supasupa_trainedtill_stage98/curr_model_stage98_entry114.35479633652099_81.02785148201173_hdg247_ep40.zip \
+  --savetraj

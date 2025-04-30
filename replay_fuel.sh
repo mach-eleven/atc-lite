b774feda63b477d194ce97848a0cad79bd16f67a
Satvik Bajpai <satvik.bajpai@plaksha.edu.in>
2025-04-30 21:41:03 +0530

    final models

--- replay_fuel.sh
+++ replay_fuel.sh
@@ -3,5 +3,5 @@ python3 replay.py --model ppo_sb3 \
  --wind-badness 10 \
  --scenario MvaGoAroundScenario \
  --wind-dirn 300 \
- --checkpoint "/Users/satvikbajpai/Downloads/sem6/rl/topgun/final_final/atc-lite/new_logs/fuel/sb3_ppo_model_10.zip" \
+ --checkpoint "new_logs/fuel/sb3_ppo_model_120.zip" \
  --skip-frames 100\
